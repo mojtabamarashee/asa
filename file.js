e8f9f109b93a02cbc918d15d5238d2ecebf18289
Mojtaba <mojtabamarashee@gmail.com>
2019-12-29 16:23:54 +0330

    98_10_08

--- file.js
+++ file.js
@@ -1085,18 +1085,18 @@ if (getSymbolsPriceHistFlag) {
 			pr.push(
 				axios
 					.get(url)
-					.then(response => {
-						console.log('histOk = ', i);
-						v.pClosingHist = response.data
-							.split(';')
-							.map(v => v.split(','))
-							.map(v => v[6])
-							.map(v => Number(v))
-							.reverse();
-					})
-					.catch(function(error) {
-						console.log('histError = ', i);
-					}),
+				//	.then(response => {
+				//		console.log('histOk = ', i);
+				//		v.pClosingHist = response.data
+				//			.split(';')
+				//			.map(v => v.split(','))
+				//			.map(v => v[6])
+				//			.map(v => Number(v))
+				//			.reverse();
+				//	})
+				//	.catch(function(error) {
+				//		console.log('histError = ', i);
+				//	}),
 			);
 		} else {
 			//console.log('histExist = ', i);
