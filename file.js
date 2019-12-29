560bf398f58cf95914972fb543d53bfd626c87b7
Mojtaba <mojtabamarashee@gmail.com>
2019-12-29 16:44:07 +0330

    98_10_08

--- file.js
+++ file.js
@@ -1083,19 +1083,18 @@ async function GetHistData() {
 		allRows.forEach((v, i) => {
 			if (v.l18.match(/^([^0-9]*)$/)) {
 				url = 'http://tsetmc.com/tsev2/chart/data/Financial.aspx?i=' + v.inscode + '&t=ph&a=1';
-				pr.push(() =>
-					axios
-						.get(url)
-						//.then(response => {
-						//	console.log('histOk = ', i);
-						//	v.pClosingHist = response.data
-						//		.split(';')
-						//		.map(v => v.split(','))
-						//		.map(v => v[6])
-						//		.map(v => Number(v))
-						//		.reverse();
-						//})
-						.catch(error => null),
+				pr.push(
+					() => axios.get(url),
+					//.then(response => {
+					//	console.log('histOk = ', i);
+					//	v.pClosingHist = response.data
+					//		.split(';')
+					//		.map(v => v.split(','))
+					//		.map(v => v[6])
+					//		.map(v => Number(v))
+					//		.reverse();
+					//})
+					//.catch(error => null),
 				);
 			} else {
 				//console.log('histExist = ', i);
