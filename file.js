c48bbaf1ccdda664ab87c41e6350824fca7fbfb9
Mojtaba <mojtabamarashee@gmail.com>
2019-12-29 16:44:49 +0330

    98_10_08

--- file.js
+++ file.js
@@ -1080,6 +1080,7 @@ maxPriceCntr = 1;
 let pr = [];
 async function GetHistData() {
 	if (getSymbolsPriceHistFlag) {
+            console.log("allRows = ", allRows.length);
 		allRows.forEach((v, i) => {
 			if (v.l18.match(/^([^0-9]*)$/)) {
 				url = 'http://tsetmc.com/tsev2/chart/data/Financial.aspx?i=' + v.inscode + '&t=ph&a=1';
